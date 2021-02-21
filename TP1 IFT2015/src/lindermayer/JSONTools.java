0d6e856fb96487688ca5bb130886a97147119d12
Abdielfer <abdielfer@gmail.com>
2021-02-20 23:19:46 -0500

    Json

--- TP1 IFT2015/src/lindermayer/JSONTools.java
+++ TP1 IFT2015/src/lindermayer/JSONTools.java
@@ -6,34 +6,66 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import lindenmayer.LSystem;
-import lindenmayer.Turtle;
+import org.json.*;
 
 
 public class JSONTools{
-	public String[] varNames;
-	public JSONObject JSonObjc;
-	public InputStream input;
 	
-	public JSONTools(String fileName)  {
+	public JSONObject JSonObjc;
+	
+	
+	public JSONTools(String fileName,LSystem S, MyTurtle T)  {
 		
 		String file = "src/"+ fileName;
 		JSonObjc = readJSONFile(file,null,null);
 	}			
 
-	public JSONObject readJSONFile(String file, LSystem S, Turtle T) throws java.io.IOException {
-        JSONObject input = new JSONObject(new JSONTokener(new java.io.FileReader(file))); // lecture de fichier JSON avec JSONTokener
-        JSONArray alphabet = input.getJSONArray("alphabet");
-        String axiom = input.getString("axiom");
-        String letter = alphabet.getString(0);
-        char sym = letter.charAt(0); // un caractère
-       // S.setAxiom(axiom);
-        
+	  public JSONObject readJSONFile(String file, LSystem S, MyTurtle T) throws java.io.IOException {
+		  	
+	   JSONObject jSonInput = new JSONObject(new JSONTokener(new java.io.FileReader("Instructions"))) ;
+		       
+       JSONArray alphabet = jSonInput.getJSONArray("alphabet");
+
+       setAxiom(jSonInput.getString("axiom"));
+
+       JSONObject rules = jSonInput.getJSONObject("rules");
+
+       for (int i = 0; i < alphabet.length(); i++) {
+           String letter = alphabet.getString(i);
+           Symbol sym = addSymbol(letter.charAt(0));
+
+           if (rules.has(letter)) {
+               JSONArray all_rules = rules.getJSONArray(letter);
+               for (int j = 0; j < all_rules.length(); j++) {
+                   addRule(sym, all_rules.getString(j));
+               }
+           }
+
+           JSONObject actions = jSonInput.getJSONObject("actions");
+
+           if (actions.has(letter)) {
+               String letterAction = actions.getString(letter);
+               setAction(sym, letterAction);
+           }
+
+       }
+
+	       JSONObject system_params = jSonInput.getJSONObject("parameters"); // tt ce qui a dans parameters
+
+	       JSONArray startJSON = system_params.getJSONArray("start"); // recupere le tableau start
+	       double start[] = new double[3];
+	       for(int i=0; i<=2; i++){
+	           start[i] = Double.parseDouble(startJSON.getString(i));
+	       }
+
+	       MyTurtle.init(new Position(start[0],start[1]),start[2]);
+
+	       double unit_step = system_params.getDouble("step");
+	       double unit_angle = system_params.getDouble("angle");
+
+	       MyTurtle.setUnits(unit_step, unit_angle);
+	    }
        return input;
-}
+   }
 	
 }
