8f763b2d6bea85f23d925df98507553cb995b42a
Abdielfer <abdielfer@gmail.com>
2021-02-20 23:17:42 -0500

    done

--- TP1 IFT2015/src/lindermayer/Main.java
+++ TP1 IFT2015/src/lindermayer/Main.java
@@ -3,5 +3,30 @@ package lindermayer;
 import java.util.ArrayList;
 
 public class Main {
-
+	public double x;
+	public double y;
+	public double ang;
+	
+	public static void main(String[] args) {
+		
+		Start(args[0]);
+		
+		
+		
+	}
+	
+	public static void Start(String s) {
+		LSystem lSyst = new LSystem();
+		MyTurtle tortue = new MyTurtle(null,null,null);
+		JSONTools jSon = new JSONTools(s,lSyst, tortue);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
