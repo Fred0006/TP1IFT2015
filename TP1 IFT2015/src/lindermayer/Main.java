985b466ad6a6b22578fd4ba080b3e4bc01fad950
Abdielfer <abdielfer@gmail.com>
2021-02-21 13:52:12 -0500

    mainJsonMyTurtle

--- TP1 IFT2015/src/lindermayer/Main.java
+++ TP1 IFT2015/src/lindermayer/Main.java
@@ -1,16 +1,21 @@
 package lindermayer;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 		public double x;
 		public double y;
 		public double ang;
 		
-		//public 
+		public static Arrays alphabet;
+		public static String axiom;
+		//public static 
+		
 		
 		public static void main(String args[]) {
 			
 			Start(args[0]);
+			
 		}
 		
 		public static void Start(String s) {
